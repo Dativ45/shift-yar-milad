40b69195f39f83ae416f37659cdbab70ef2cd4d4
Dativ45 <dativ67@gmail.com>
2026-09-18 15:50:14 +0330

    Create milad_wards.dart

---
+++ lib/data/milad_wards.dart
@@ -0,0 +1,8 @@
+const List<Map<String,dynamic>> miladWards = [
+  {"id":1,"name":"اورژانس","icon":"🚨"}, {"id":2,"name":"اورژانس قلب","icon":"❤️"},
+  {"id":3,"name":"ICU عمومی","icon":"🏥"}, {"id":4,"name":"ICU جراحی","icon":"🩺"},
+  {"id":5,"name":"CCU","icon":"💓"}, {"id":6,"name":"داخلی","icon":"🩻"},
+  {"id":7,"name":"زنان و زایمان","icon":"🤰"}, {"id":8,"name":"اطفال","icon":"🧒"},
+  {"id":9,"name":"NICU","icon":"👼"}, {"id":10,"name":"اتاق عمل","icon":"🏨"},
+  {"id":11,"name":"رادیولوژی","icon":"☢️"}, {"id":12,"name":"آزمایشگاه","icon":"🧫"},
+];
